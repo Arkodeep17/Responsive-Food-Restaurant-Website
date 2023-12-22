09ed5aeb671381e9df7ee69a9042e0ff5bfe55db
Arkodeep Dey <65350545+Arkodeep17@users.noreply.github.com>
2023-12-22 17:47:41 +0530

    Add files via upload

---
+++ Js pg/script.js
@@ -0,0 +1,18 @@
+let menu=document.querySelector('#menu-bars');
+let navbar=document.querySelector('.navbar');
+
+menu.onclick =() => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+
+}
+
+function loader(){
+	document.querySelector(".loader-container").classList.add('fade-out');
+
+}
+function fadeOut(){
+	setInterval(loader,3000);
+}
+window.onload=fadeOut;
+
